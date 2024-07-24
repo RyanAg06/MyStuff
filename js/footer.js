c0dafefa8296f7366c9e7641b384c3e6a1a409cc
RyanAg01 <brianjkh1@gmail.com>
2024-07-24 14:38:35 -0700

    Se Modifico el Footer

--- js/footer.js
+++ js/footer.js
@@ -10,18 +10,30 @@ document.write(`
                 <ion-icon name="logo-facebook" class="icono-footer"></ion-icon>
                 <a href="https://www.facebook.com/profile.php?id=100082157757979" target="_blank">Facebook</a>
             </li>
-            
-            <!-- Twitter -->
+
+            <!-- Discord -->
             <li>
-                <ion-icon name="logo-twitter"></ion-icon>
-                <a href="https://twitter.com/_ryan_ag" target="_blank">Twitter (X)</a>
+                <ion-icon name="logo-discord"></ion-icon>
+                <a href="https://Discordapp.com/users/1096676312862695455" target="_blank">Discord</a>
+            </li>
+            
+            <!-- Steam -->
+            <li>
+                <ion-icon name="logo-steam"></ion-icon>
+                <a href="https://steamcommunity.com/id/ryanag/" target="_blank">Steam</a>
+            </li>
+
+            <!-- GitHub -->
+            <li>
+                <ion-icon name="logo-github"></ion-icon>
+                <a href="https://github.com/RyanAg06" target="_blank">GitHub</a>
             </li>
 
         </ul>
 			
         <!-- Creditos -->
 		<div class="footer__creditos">
-            <b>Hecho por RyanAg ©</b>
+            <b>MyStuff by RyanAg</b>
 		</div>
 
         <!-- Iconos -->
