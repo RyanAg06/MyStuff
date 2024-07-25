b4dc81d03a90703fd0cc10afdc6ef087eadb4432
RyanAg01 <brianjkh1@gmail.com>
2024-07-24 23:37:07 -0700

    Se Creo el Slider

--- js/funciones.js
+++ js/funciones.js
@@ -5,7 +5,6 @@ window.onload = function codigoJS()
 {
     abrirMenuLateral();
     abrirSubMenus();
-    colorDominante("noticia1");
 }
 
 /* ========================================= FUNCIONES ========================================= */
@@ -71,7 +70,7 @@ function cerrarSubmenus()
     flechas.forEach(a=>
     {
         a.classList.remove("activo");           // Regreso las Flechas a su Estado Desactivado
-    })
+    });
 
     submenu2.forEach(a =>
     {
@@ -107,4 +106,97 @@ function colorDominante(imagen)
     let img = document.getElementById(`${imagen}`);         // Selecciono la Imagen de Donde se Obtendra el Color Dominante
     let color = colorThief.getColor(img);                   // Uso Metodo de Objeto y Obtengo Color Dominante
     console.log(`El color dominante de la ${imagen} es: rgb(${color[0]}, ${color[1]}, ${color[2]})`);       // Muestro en Consola el Color Dominante
+}
+
+
+// ====================================== SLIDER ==========================================
+
+// Variables
+let contenedores = document.querySelectorAll(".slider__imagenes");                  // Selecciono Contenedores
+let sliderActual = "";                                                              // Reinicio Slider Seleccionado
+let margin = 0;                                                                     // Reinicio Margin
+
+// Redimensiono Slider
+contenedores.forEach(contenedor =>
+{
+    let imagenes = contenedor.querySelectorAll(".slider__imagen");                      // Selecciono Cada Imagen del Contenedor
+    cantidad = imagenes.length;                                                     // Obtengo Numero de Imagenes
+    contenedor.style.width = `${100 * cantidad}%`;                                  // Redimensiono Contenedor por Cada Imagen
+    contenedor.style.marginLeft = `0%`;                                             // Voy a la Ultima Imagen
+
+    imagenes.forEach(imagen =>
+    {
+        imagen.style.width = `${100 / cantidad}%`;                                  // Redimensiono Cada Imagen del Contenedor
+    });
+});
+
+// Funcion Mover Derecha Slider
+function moverDerecha(tarjeta__slider)
+{
+    let tarjeta = document.getElementById(`${tarjeta__slider}`);            // Selecciono La Tarjeta
+    let sliderImagenes = tarjeta.querySelector(".slider__imagenes");        // Selecciono Conenedor de Imagenes
+    let imagenes = tarjeta.querySelectorAll(".slider__imagen");             // Selecciono Todas las Imagenes
+    let cantidad = imagenes.length;                                         // Cuento Nuemro de Imagenes
+
+    if(sliderActual === tarjeta__slider)                                    // Verifico si Selecciono Otro Slider
+    {
+        margin -= 100;                                                      // Muevo a la Derecha
+        if(margin < -((cantidad - 1) * 100))                                // Verifico si Llego al Final
+        {
+            margin = -((cantidad - 1) * 100);                               // Evito que se Pase
+        }
+    }
+    else
+    {
+        sliderActual = tarjeta__slider;                                     // Cambio de Slider
+        if(sliderImagenes.style.marginLeft == "0%")                         // Verifico si Esta en la Primera Posicion
+        {
+            margin = 0;                                                     // Reinicio Margin
+            margin -= 100;                                                  // Muevo a Derecha
+        }
+        else
+        {
+            margin = parseInt(sliderImagenes.style.marginLeft.replace("%",""));     // Obtengo Margin Actual y lo Convierto a Numero
+            margin -= 100;                                                          // Muevo a la Derecha
+            if(margin < -((cantidad - 1) * 100))                                    // Verifico si Llego al Final
+            {
+                margin = -((cantidad - 1) * 100);                                   // Evito que se Pase
+            }
+        }
+    }
+    sliderImagenes.style.marginLeft = `${margin}%`;                                 // Aplico Margin al Contenedor de Imagenes
+}
+
+// Funcion Mover Izquierda Slider
+function moverIzquierda(tarjeta__slider)
+{
+    let tarjeta = document.getElementById(`${tarjeta__slider}`);            // Selecciono La Tarjeta
+    let sliderImagenes = tarjeta.querySelector(".slider__imagenes");        // Selecciono Conenedor de Imagenes
+
+    if(sliderActual === tarjeta__slider)                                    // Verifico si Selecciono Otro Slider
+    {
+        margin += 100;                                                      // Muevo a la Derecha
+        if(margin >= 0)                                                     // Verifico si Llego al Final
+        {
+            margin = 0;                                                     // Evito que se Pase
+        }
+    }
+    else
+    {
+        sliderActual = tarjeta__slider;                                     // Cambio de Slider
+        if(sliderImagenes.style.marginLeft == "0%")                         // Verifico si Esta en la Primera Posicion
+        {
+            margin = 0;                                                     // Reinicio Margin
+        }
+        else
+        {
+            margin = parseInt(sliderImagenes.style.marginLeft.replace("%",""));     // Obtengo Margin Actual y lo Convierto a Numero
+            margin += 100;                                                          // Muevo a la Derecha
+            if(margin >= 0)                                                         // Verifico si Llego al Final
+            {
+                margin = 0;                                                         // Evito que se Pase
+            }
+        }
+    }
+    sliderImagenes.style.marginLeft = `${margin}%`;                                 // Aplico Margin al Contenedor de Imagenes
 }
